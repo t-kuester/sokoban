6a21fff55c69361c72418f0fa573786ea6bc1be2
tkuester <tobias.kuester@gmx.net>
2015-03-19 21:13:06 +0100

    added sokoban game

---
+++ sokoban.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+"""
+Sokoban Game Model, by Tobias Kuester, 2014
+
+This modules provides a class encapsulating the current state of the Sokoban 
+game as well as some helper methods for loading game files and other stuff.
+
+The game model provides basic functionality for movement and for pushing boxes,
+for path planning and for planning to push a box to a given location.
+"""
+
+import collections
+
+WALL        = '#'
+PLAYER      = '@'
+PLAYER_GOAL = '+'
+BOX         = '$'
+BOX_GOAL    = '*'
+FLOOR_GOAL  = '.'
+FLOOR       = ' '
+COMMENT     = ';'
+
+
+def is_goal(symbol):
+	"""return whether the given symbol represents a goal tile"""
+	return symbol in (PLAYER_GOAL, BOX_GOAL, FLOOR_GOAL)
+
+def is_free(symbol):
+	"""return whether the given symbol represents passable space"""
+	return symbol in (FLOOR, FLOOR_GOAL)
+	
+def is_player(symbol):
+	"""return whether the given symbol represents a player"""
+	return symbol in (PLAYER, PLAYER_GOAL)
+	
+def is_box(symbol):
+	"""return whether the given symbol represents a box"""
+	return symbol in (BOX, BOX_GOAL)
+
+def load_levels(filename):
+	"""Load levels from given level file. Levels are separated by empty or
+	commented lines. If the levels can not be loaded, None is returned.
+	"""
+	with open(filename) as levelfile:
+		levels = [[]]
+		for line in levelfile.read().splitlines():
+			if line.lstrip().startswith('#'):
+				levels[-1].append(line)
+			elif levels[-1]:
+				levels.append([])
+
+		return tuple(map(tuple, filter(None, levels)))
+
+
+class SokobanGame:
+	"""Class representing the current state of the Sokoban game.
+	
+	This class encapsulates the entire level set, the current level, the current
+	position of the player, and a list of the moves so far taken.
+	"""
+	
+	def __init__(self, levels):
+		"""Create new Sokoban game instance.
+		"""
+		self.levels = levels
+		self.number = 0
+		self.load_level()
+		self.backup = None
+		
+	def load_level(self, number=None):
+		"""Load level with given number from current level set.
+		If no number is given, reload the current level.
+		"""
+		self.number = number if number is not None else self.number
+		self.state = map(list, self.levels[self.number % len(self.levels)])
+		self.progress = []
+		self.r = self.c = 0
+		for r, row in enumerate(self.state):
+			for c, col in enumerate(row):
+				if is_player(self.state[r][c]):
+					self.r, self.c = r, c
+		if number:
+			self.backup = None
+
+	def save(self):
+		"""Save current state of the game.
+		"""
+		self.backup = map(list, self.state), self.r, self.c, list(self.progress)
+		
+	def load(self):
+		"""Restore previously saved game state.
+		"""
+		if self.backup:
+			self.state, self.r, self.c, self.progress = self.backup
+			self.save()
+	
+	def get_rel(self, dr, dc):
+		"""Get symbol at row/column relative to player's position.
+		"""
+		return self.state[self.r + dr][self.c + dc]
+	
+	def set_rel(self, dr, dc, symbol):
+		"""Set symbol at row/column relative to player's position.
+		"""
+		self.state[self.r + dr][self.c + dc] = symbol
+	
+	def can_move(self, dr, dc, push=False):
+		"""Check whether player can move to an adjacent location, with or without
+		being allowed to push a box.
+		"""
+		next = self.get_rel(dr, dc)
+		if is_free(next):
+			return True
+		elif push and is_box(next):
+			return is_free(self.get_rel(2 * dr, 2 * dc))
+		return False
+	
+	def move(self, dr, dc, push=False):
+		"""Move to an adjacent location, after checking whether that move would 
+		be legal, with or without being allowed to move a box.
+		"""
+		if self.can_move(dr, dc, push):
+			cur = self.get_rel(0, 0)
+			next = self.get_rel(dr, dc)
+			self.set_rel(0, 0, FLOOR_GOAL if is_goal(cur) else FLOOR)
+			self.set_rel(dr, dc, PLAYER_GOAL if is_goal(next) else PLAYER)
+			if push and is_box(next):
+				nextnext = self.get_rel(2 * dr, 2 * dc)
+				self.set_rel(2 * dr, 2 * dc, BOX_GOAL if is_goal(nextnext) else BOX)
+			self.r += dr
+			self.c += dc
+			self.progress.append((dr, dc))
+			return True
+		else:
+			return False
+
+	def find_path(self, row, col):
+		"""Try to find a path to (row, col), without moving boxes, using BFS.
+		"""
+		queue = collections.deque([(self.r, self.c, [])])
+		visited = set()
+		while queue:
+			(r, c, path) = queue.popleft()
+			if (r, c) in visited:
+				continue
+			visited.add((r, c))
+			if (r, c) == (row, col):
+				return path
+			for dr, dc in ((0,+1), (0,-1), (-1,0), (+1,0)):
+				if is_free(self.state[r + dr][c + dc]):
+					queue.append((r + dr, c + dc, path + [(dr, dc)]))
+		return None
+	
+	def plan_push(self, start, goal):
+		"""Plan how to push box from start to goal position. Planning is done
+		using the same basic breadth-first-search as for movement planning, 
+		except that we have to keep track of the actual game state, which is 
+		done by simply replaying the so-far path in each step. Not terribly
+		efficient, but fast enough, simple and robust. Positioning of the player
+		is done using the basic movement planning algorithm.
+		"""
+		original_state = tuple(map(tuple, self.state))
+		original_position = self.r, self.c
+		original_progress = list(self.progress)
+
+		result = None
+		queue = collections.deque([(start, [])])
+		visited = set()
+		while queue:
+			(r, c), path = queue.popleft()
+
+			if (r, c) in visited:
+				continue
+			visited.add((r, c))
+
+			if (r, c) == goal:
+				result = path
+				break
+			
+			self.state = map(list, original_state)
+			self.r, self.c = original_position
+			self.replay(path, False)
+		
+			for dr, dc in ((0,+1), (0,-1), (-1,0), (+1,0)):
+				target = self.state[r + dr][c + dc]
+				if is_free(target) or is_player(target):
+					positioning = self.find_path(r - dr, c - dc)
+					if positioning is not None:
+						queue.append(((r + dr, c + dc), path + positioning + [(dr, dc)]))
+
+		self.state = map(list, original_state)
+		self.r, self.c = original_position
+		self.progress = original_progress
+		return result
+	
+	def replay(self, steps, reset):
+		"""Resets the game state and replay the moves given in the list.
+		"""
+		if reset:
+			self.load_level()
+		for dr, dc in steps:
+			self.move(dr, dc, True)
+			
+	def check_solved(self):
+		"""Check whether all goal tiles have a box placed on them.
+		"""
+		return not any(symbol in (PLAYER_GOAL, FLOOR_GOAL) 
+		               for row in self.state for symbol in row)
+	
+	def __str__(self):
+		"""Create simple string representation of the game state."""
+		return "SokobanGame(level: %d/%d, position: %r, progress: %r)" % \
+				(self.number, len(self.levels), (self.r, self.c), self.progress)
+
+# testing	
+if __name__ == "__main__":
+	levels = load_levels("levels/microban.txt")
+	print "Number of levels:", len(levels)
+	for line in levels[0]: print line
+	print SokobanGame(levels)
---
+++ sokoban_game.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+"""
+Simple Sokoban Game, by Tobias Küster, 2014
+
+Features:
+- load Sokoban levels in standard sokoban level file format
+- save progress (solved/unsolved, number of turns) for each level in JSON file
+- controls with arrow-keys or with mouse
+- save/restore snapshot of current level, undo-stack
+- path-planning for movement and push-planning for pushing single boxes
+
+Controls:
+- Arrow Keys: Move/Push
+- Mouse Click: Fast-move, push-planning
+- q: quit, r: reload level, u: undo
+- s: save snapshot, l: load snapshot
+- PgUp, PgDn: Next/Previous Level
+"""
+
+import sokoban
+import Tkinter as tk
+import json
+import time
+
+DIRECTIONS = {"Right": (0,+1), "Left": (0,-1), "Up": (-1,0), "Down": (+1,0)}
+DIRECTIONS_INV = dict((v, k) for (k, v) in DIRECTIONS.items())
+
+class SokobanFrame(tk.Frame):
+	"""Sokoban Game Frame.
+	
+	This class provides the graphical interface to the Sokoban game, including
+	keyboard and mouse control, loading level sets, undo, etc.
+	"""
+	
+	def __init__(self, game, scores):
+		"""Create new Sokoban Frame instance, using a given game instance and
+		a dictionary holding the scores for the individual levels.
+		"""
+		tk.Frame.__init__(self, None)
+		self.master.title("Sokoban")
+		self.game = game
+		self.scores = scores
+		self.pack(fill=tk.BOTH, expand=tk.YES)
+		
+		self.selected = None
+
+		self.canvas = tk.Canvas(self)
+		self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
+		
+		self.status = tk.Label(self)
+		self.status.pack(side=tk.LEFT)
+		
+		self.history = tk.Label(self)
+		self.history.pack(side=tk.RIGHT)
+		
+		self.bind_all("<KeyPress>", self.handle_keys)
+		self.bind_all("<Shift-KeyPress>", lambda e: self.handle_keys(e, True))
+		self.canvas.bind("<ButtonRelease>", self.handle_mouse)
+		self.bind("<Configure>", self.draw_state)
+		self.update_state()
+		
+	def handle_keys(self, event, shift=False):
+		"""Handle key events, e.g. or movement, save, load, undo, restart, etc.
+		"""
+		self.selected = None
+		if event.keysym == "q":
+			self.quit()
+		if event.keysym == "r":
+			self.game.load_level()
+		if event.keysym == "u":
+			steps = self.game.progress[:-1]
+			self.game.replay(steps, True)
+		if event.keysym == "s":
+			self.game.save()
+		if event.keysym == "l":
+			self.game.load()
+		if event.keysym in ("Prior", "Next"):
+			inc = lambda num: (num +1 if event.keysym == "Next" else num-1) % len(self.game.levels)
+			number = inc(self.game.number)
+			# shift: fast-forward to next unsolved level, if any
+			while shift and self.scores[number] and number != self.game.number:
+				number = inc(number)
+			self.game.load_level(number )
+		if event.keysym in DIRECTIONS:
+			dr, dc = DIRECTIONS[event.keysym]
+			while self.game.move(dr, dc, not shift) and shift:
+				# self.after(20, self.handle_keys, event, shift)
+				pass
+		self.update_state()
+		
+	def handle_mouse(self, event):
+		"""Handle mouse events for planning movement and box-pushing,
+		"""
+		w = self.get_cellwidth()
+		r, c = event.y / w, event.x / w
+		
+		try:
+			symbol = self.game.state[r][c]
+			if sokoban.is_box(symbol):
+				self.selected = (r, c)
+			if sokoban.is_free(symbol) or sokoban.is_player(symbol):
+				if self.selected is None:
+					path = self.game.find_path(r, c)
+					self.move_path(path, False)
+				else:
+					path = self.game.plan_push(self.selected, (r, c))
+					self.move_path(path, True)
+				self.selected = None
+			self.update_state()
+		except IndexError:
+			pass
+
+	def move_path(self, path, push):
+		"""Move one step in the given path, then wait a short time, repaint, and
+		continue.
+		"""
+		if path:
+			dr, dc = path[0]
+			self.game.move(dr, dc, push)
+			self.after(25, self.move_path, path[1:], push)
+		self.update_state()
+
+	def update_state(self):
+		"""Check the current state of the game (solved or not, number of steps, 
+		etc.), update the status line, and redraw the canvas.
+		"""
+		num = self.game.number
+		solved = self.game.check_solved()
+		turns = len(self.game.progress)
+		if solved and (self.scores[num] is None or turns < self.scores[num]):
+			self.scores[num] = turns
+		status = "%d/%d, %d Steps (Best: %r)" % (num + 1, 
+				len(self.game.levels), turns, self.scores[num])
+		history = " ".join(DIRECTIONS_INV[d][0] for d in self.game.progress[-30:])
+		self.status.configure(text=status)
+		self.history.configure(text=history)
+		self.draw_state()
+		
+	def draw_state(self, event=None):
+		"""Draw the current state of the game to the canvas.
+		"""
+		self.update()
+		self.canvas.delete("all")
+		w = self.get_cellwidth()
+		for r, row in enumerate(self.game.state):
+			for c, col in enumerate(row):
+				cell = self.game.state[r][c]
+				x, y = c*w, r*w
+				if cell == sokoban.WALL:
+					self.canvas.create_rectangle(x, y, x+w, y+w, fill="#888888")
+				if sokoban.is_goal(cell):
+					self.canvas.create_oval(x+w*.1, y+w*.1, x+w*.9, y+w*.9, fill="#88CC88")
+				if sokoban.is_box(cell):
+					if (r, c) == self.selected:
+						self.canvas.create_rectangle(x, y, x+w, y+w, fill="#CC8888")
+					self.canvas.create_rectangle(x+w*.2, y+w*.2, x+w*.8, y+w*.8, fill="#CCCC88")
+				if sokoban.is_player(cell):
+					self.canvas.create_oval(x+w*.3, y+w*.3, x+w*.7, y+w*.7, fill="#8888CC")
+
+	def get_cellwidth(self):
+		"""Simple helper method for getting the optimal width for a cell.
+		"""
+		width, height = self.canvas.winfo_width(), self.canvas.winfo_height()
+		return min(height / len(self.game.state), 
+		           width  / max(map(len, self.game.state)))
+
+
+if __name__ == "__main__":
+	import optparse
+	parser = optparse.OptionParser("sokoban_game.py [level file]")
+	(options, args) = parser.parse_args()
+	
+	# load level file given as parameter
+	levelfile = (args or ["levels/masmicroban.txt"])[0]
+	levels = sokoban.load_levels(levelfile)
+	
+	#load highscores for level file
+	savesfile = "sokoban_saves.json"
+	try:
+		with open(savesfile) as f:
+			gamestate = json.load(f)
+	except:
+		gamestate = {}
+	scores = gamestate.get(levelfile, [None] * len(levels))
+
+	# start game
+	game = sokoban.SokobanGame(levels)
+	frame = SokobanFrame(game, scores)
+	frame.mainloop()
+
+	# save highscores
+	gamestate[levelfile] = scores
+	with open(savesfile, 'w') as f:
+		json.dump(gamestate, f)
